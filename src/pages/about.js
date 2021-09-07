5055c10c20f125219ae707463389c03a5422b293
jordaneaster <jordaneaster222@icloud.com>
2021-09-07 19:44:41 -0400

    first gatsby site

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Layout from "../../components/layout";
+const AboutPage = () =>{
+    return (
+        <Layout pageTitle="About Me">
+        <main>
+            <nav>
+            </nav>
+            <title>About Me</title>
+            <h1>I am a creative at heart and soul. I am a strategist.
+                I am a father. I am a husband. I am an innovator. I am Jordan. 
+                Thank you for visiting my site!
+            </h1>
+        </main>
+        </Layout>
+       
+    )
+}
+export default AboutPage
