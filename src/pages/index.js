e424487516747d845284603071ff8d55861dee5d
jordaneaster <jordaneaster222@icloud.com>
2021-09-08 10:46:01 -0400

    finished tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
-import React from "react";
+import * as React from "react";
 import Layout from "../../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 const IndexPage = () =>{
     return (
         <Layout pageTitle="Home Page">
  <h1>Welcome to my site!</h1>
         <p>My name is Jordan Easter.. and I am a developer.</p>
+        
         <h2>Here is a list of what languages and frameworks I use:</h2>
         <ul>
             <li>Java - Spring [Boot/MongoDB]</li>
             <li>JavaScript - SPA</li>
             <li>React</li>
         </ul>
+        <StaticImage
+        alt="a beautiful body of water"
+        src="https://images.unsplash.com/photo-1615393915491-2b7cb33b2678?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFrZSUyMGRpc3RyaWN0JTIwdWt8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        />
         </Layout>
        
     )
