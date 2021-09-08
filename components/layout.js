ea2313db29780b0580566807f1e14cdcc6b38aeb
jordaneaster <jordaneaster222@icloud.com>
2021-09-08 12:29:28 -0400

    blog page/photos now rendering

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import {
   container,
   heading,
@@ -9,6 +9,7 @@ import {
 } from '/components/layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+
   return (
     <div className={container}>
       <title>{pageTitle}</title>
@@ -23,6 +24,11 @@ const Layout = ({ pageTitle, children }) => {
             <Link to="/about" className={navLinkText}>
               About
             </Link>
+          </li>
+          <li className={navLinkItem}>
+              <Link to="/blog" className={navLinkText}>
+                  Blog
+              </Link>
           </li>
         </ul>
       </nav>
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,14 @@ module.exports = {
     title: "My First Gatsby Site",
   },
   plugins: [
-    "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
